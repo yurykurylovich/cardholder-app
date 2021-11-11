7c4cb1a347b55e4659c479fbe4cc589d80b31081
yurykurylovich <yury.kurylovich@gmail.com>
2021-11-11 23:13:03 +0300

    Add modals

--- js/main.js
+++ js/main.js
@@ -23,8 +23,8 @@ function createCardElement(id, content) {
   card.classList.add("bank-card");
   card.innerHTML = `
     <div class="card-buttons">
-      <button class="edit-card" id="editCard">Edit</button>
-      <button class="delete-card" id="deleteCard">Delete</button>
+      <button class="edit-card" id="editCard"><i class="fas fa-edit"></i></button>
+      <button class="delete-card" id="deleteCard"><i class="fas fa-trash"></i></button>
     </div>
     <div class="card-title">Card Title</div>
     <div class="card-number">
@@ -45,34 +45,25 @@ function createCardElement(id, content) {
   // })
 
   card.addEventListener("click", (event) => {
-    // const doDelete = confirm(
-    //   "Are you sure you wish to delete this sticky card?"
-    // );
-    //
-    // if (doDelete) {
-    //   deleteCard(id, element);
-    // }
-    console.log("Clicked")
 
-    if (event.target.matches(".delete-card")) {
-      console.log("Delete card")
+    if (event.target.matches(".fa-trash")) {
       showDeleteModal(id, card)
+    } else if (event.target.matches(".fa-edit")) {
+      console.log("EDIT CARD PRESSED")
+      showCardModal(id, card)
     }
   });
-
-  // card.addEventListener("dblclick", () => {
-  //   console.log("Double Clicked")
-  //   showDeleteModal(id, element)
-  // })
 
   return card;
 }
 
-function addCard() {
+function addCard(cardInfo = {}) {
   const cards = getCards();
   const cardObject = {
     id: Math.floor(Math.random() * 100000),
-    content: ""
+    content: cardInfo.title || '',
+    cardNumber: cardInfo.number || '',
+    cardDescription: cardInfo.description || '',
   };
 
   const cardElement = createCardElement(cardObject.id, cardObject.content);
@@ -91,7 +82,6 @@ function updateCard(id, newContent) {
 }
 
 function deleteCard(id, element) {
-  console.log("Delete card")
   const cards = getCards().filter((card) => card.id !== id);
 
   saveCards(cards);
@@ -104,9 +94,9 @@ function showDeleteModal(id, card) {
 
   modal.classList.add("modal")
   modal.innerHTML = `
-    <div class="modal__inner">
-      <div class="modal__content">Are you sure you want to delete this card?</div>
-      <div class="modal__bottom"></div>
+    <div class="delete__modal__inner">
+      <div class="delete__modal__content">Are you sure you want to delete this card?</div>
+      <div class="delete__modal__bottom"></div>
     </div>
   `;
   document.body.appendChild(modal)
@@ -137,6 +127,101 @@ function showDeleteModal(id, card) {
       button.onClick(modal);
     });
 
-    modal.querySelector(".modal__bottom").appendChild(element);
+    modal.querySelector(".delete__modal__bottom").appendChild(element);
   }
+}
+
+function showCardModal(id, card) {
+  const modal = document.createElement("div");
+  modal.classList.add("modal")
+  modal.innerHTML = `
+    <div class="card__modal__inner">
+      <form id="card__form" class="form">
+        <div class="form-control">
+          <label for="title">Title</label>
+          <input type="text" placeholder="Card title" id="title"/>
+        </div>
+        <div class="form-control">
+          <label for="number">Card number</label>
+          <input type="text" placeholder="Description" id="number"/>
+          <small>Error message</small>
+        </div>
+        <div class="form-control">
+          <label for="card-description">Description</label>
+          <textarea type="text" placeholder="Description" id="card-description" maxlength="1024"></textarea>
+        </div>
+        <div class="card__modal__bottom"></div>
+      </form>
+    </div>
+  `;
+  document.body.appendChild(modal);
+
+  const form = document.getElementById('card__form');
+  const title = document.getElementById('title');
+  const number = document.getElementById('number');
+  const description = document.getElementById('description');
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    if(checkInputs() === true) {
+
+    }
+  });
+
+  function checkInputs() {
+    const titleValue = title.value.trim();
+    const numberValue = number.value.trim();
+    const descriptionValue = description.value.trim();
+
+    if(!isCardNumber(numberValue)) {
+      setErrorFor(number, 'Card number should contain 16 numbers');
+      return false;
+    }
+
+    return true;
+  }
+
+  function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
+  }
+
+  function isCardNumber(cardNumber) {
+    return /^([0-9]{16})$/.test(cardNumber)
+  }
+
+
+
+  const buttons = [
+    {
+      label: "Cancel",
+      onClick: (modal) => {
+        document.body.removeChild(modal);
+      },
+    },
+    {
+      label: "Save",
+      onClick: (modal) => {
+        // deleteCard(id, card)
+        document.body.removeChild(modal);
+      },
+    }
+  ];
+
+  for (const button of buttons) {
+    const element = document.createElement("button");
+
+    element.setAttribute("type", "button");
+    element.classList.add("modal__button");
+    element.textContent = button.label;
+    element.addEventListener("click", () => {
+      button.onClick(modal);
+    });
+
+    modal.querySelector(".card__modal__bottom").appendChild(element);
+  }
+
 }
